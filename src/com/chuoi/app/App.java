b14fd1d3523ebafaeb4cd39d39a6e534444e526e
gerpan <nhaths4701gmail.com>
2021-05-12 14:16:55 +0700

    thêm phần đăng nhập

--- src/com/chuoi/app/App.java
+++ src/com/chuoi/app/App.java
@@ -1,10 +1,10 @@
 package com.chuoi.app;
 
+import com.chuoi.models.AccountDao;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("Hello chuoi");
 	}
-
 }
