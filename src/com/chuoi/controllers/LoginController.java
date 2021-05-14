5a20a7162edc1703928b01166a196552755d9dac
ninhTKieen <ninhtrungkien2811@gmail.com>
2021-05-14 15:52:53 +0700

    update views and controller

---
+++ src/com/chuoi/controllers/LoginController.java
@@ -0,0 +1,40 @@
+package com.chuoi.controllers;
+import com.chuoi.views.LoginView;
+import com.chuoi.services.AccountService;
+import com.chuoi.models.Account;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginController {
+	private AccountService accountService;
+	private LoginView loginView;
+	
+	public LoginController(LoginView loginView)
+	{
+		this.loginView=loginView;
+		this.accountService=new AccountService();
+		loginView.addLoginListener(new LoginListener());
+	}
+	
+	public void showLoginView()
+	{
+		loginView.setFrameVisible();
+	}
+	
+	class LoginListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) {
+			Account account = loginView.getUser();
+			if(!accountService.checkAdminAccount(account.getUsername(), account.getPassword()))
+			{
+				loginView.showMess("Wrong Username or Password!Try again");
+			}
+			else
+			{
+				loginView.showMess("Naice");
+			}
+		}
+		
+	}
+}
