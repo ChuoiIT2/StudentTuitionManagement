dbd505398fe9ea7f17235056fa5dd476308fdb60
gerpan <nhaths4701gmail.com>
2021-05-20 08:47:48 +0700

    them look and feel material ui

--- src/com/chuoi/app/App.java
+++ src/com/chuoi/app/App.java
@@ -1,11 +1,24 @@
 package com.chuoi.app;
 
+import mdlaf.*;
+import mdlaf.themes.*;
+
+import javax.swing.*;
 import com.chuoi.views.LoginView;
 import com.chuoi.controllers.LoginController;
 import com.chuoi.configs.Constant;
-public class App {
 
+public class App {
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(new MaterialLookAndFeel());
+			if (UIManager.getLookAndFeel() instanceof MaterialLookAndFeel) {
+				MaterialLookAndFeel.changeTheme(new MaterialLiteTheme());
+			}
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+
 		LoginView loginView = new LoginView();
 		LoginController loginController = new LoginController(loginView);
 		loginController.showLoginView(Constant.DEFAULT_FRAME_SHOW);
